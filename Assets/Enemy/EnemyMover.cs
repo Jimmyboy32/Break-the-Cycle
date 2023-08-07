723bb9c36bcafd47d6a26d6f22817472d106283c
Jim Christin <jimchristin1@outlook.com>
2023-08-07 12:16:28 -0700

    Smooth enemy movement

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -6,7 +6,7 @@ public class EnemyMover : MonoBehaviour
 {
     // Creation of a list for the multiple waypoints of the grid
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
   
     void Start()
     {
@@ -17,10 +17,22 @@ public class EnemyMover : MonoBehaviour
     IEnumerator FollowPath()
     {
         // For all the waypoints in the established trajectory
+        // Smooth movement for starting point to destination
         foreach(Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            //Will repeat as long as destination has not been reached (travelPercent < 1f)
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 
